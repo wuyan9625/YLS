546e8167c6ca4453d57506be8725351ad76ef0c9
wuyan9625 <wuyan9625@gmail.com>
2025-04-19 15:54:02 +0800

    Update location_webhook.py

--- location_webhook.py
+++ location_webhook.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, request, jsonify
 import sqlite3
 from datetime import datetime
+import pytz
 
 location_bp = Blueprint("location", __name__)
 DB_PATH = 'checkin.db'
+tz = pytz.timezone("Asia/Taipei")
 
 @location_bp.route("/webhook", methods=["POST"])
 def receive_location():
@@ -12,11 +14,23 @@ def receive_location():
         line_id = data.get("line_id")
         latitude = float(data.get("latitude"))
         longitude = float(data.get("longitude"))
-        timestamp = data.get("timestamp", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+        # 嘗試取得 timestamp，若有傳入則解析為台灣時間，否則使用現在時間
+        if "timestamp" in data:
+            try:
+                utc_time = datetime.fromisoformat(data["timestamp"].replace("Z", "+00:00"))
+                local_time = utc_time.astimezone(tz)
+            except:
+                local_time = datetime.now(tz)
+        else:
+            local_time = datetime.now(tz)
+
+        timestamp_str = local_time.strftime("%Y-%m-%d %H:%M:%S")
 
         conn = sqlite3.connect(DB_PATH)
         cursor = conn.cursor()
 
+        # 查詢綁定使用者
         cursor.execute("SELECT employee_id, name FROM users WHERE line_id=?", (line_id,))
         row = cursor.fetchone()
 
@@ -25,10 +39,11 @@ def receive_location():
 
         employee_id, name = row
 
+        # 寫入定位紀錄
         cursor.execute('''
             INSERT INTO location_logs (line_id, employee_id, name, latitude, longitude, timestamp)
             VALUES (?, ?, ?, ?, ?, ?)
-        ''', (line_id, employee_id, name, latitude, longitude, timestamp))
+        ''', (line_id, employee_id, name, latitude, longitude, timestamp_str))
 
         conn.commit()
         conn.close()
